bb308e3ffb00bc6d0b2fa55cb77882d9276c55c4
Morten Laukvik <morten@laukvik.no>
2017-10-15 03:34:45 +0200

    Added scope

--- src/Scope.js
+++ src/Scope.js
@@ -3,9 +3,39 @@
  */
 export class Scope {
 
+  /**
+   * Returns an array of all scopes in the string
+   *
+   * @param value
+   * @returns {string}
+   */
+  static findScopes(value){
+    let newValue = "";
+    let afterStart = false;
+    let variable = "";
+    let scopesArr = [];
+    for (let x=0; x<value.length; x++) {
+      let c = value.charAt(x);
+      if (c === '{') {
+        if (afterStart) {
+          variable = "{";
+        }
+        afterStart = true;
+        variable = "{";
+      } else if (c === '}' && afterStart) {
+        variable += c;
+        scopesArr.push(Scope.parseScope(variable));
+        variable = "";
+      } else if (afterStart) {
+        variable += c;
+      } else {
+        newValue += c;
+      }
+    }
+    return scopesArr;
+  }
 
-
-  parseScope(value) {
+  static parseScope(value) {
     let isString = typeof value === 'string';
     if (!isString) {
       return {
--- src/__tests__/Scope-spec.js
+++ src/__tests__/Scope-spec.js
@@ -2,22 +2,41 @@ import {Scope,SCOPES} from '../Scope';
 
 describe('Scope', () => {
 
+  describe('findScopes', () => {
+    it('should find all scopes', () => {
+      let scopes = Scope.findScopes("/category/{global:selectedMovie}/{id}");
+      expect(scopes.length).toBe(2);
+      let s1 = scopes[0];
+      expect(s1.scope).toBe(SCOPES.GLOBAL);
+      expect(s1.value).toBe("selectedMovie");
+      let s2 = scopes[1];
+      expect(s2.scope).toBe(SCOPES.VIEW);
+      expect(s2.value).toBe("id");
+    });
+
+    it('should handle error', () => {
+      let scopes = Scope.findScopes("/category/{{selectedMovie}");
+      expect(scopes.length).toBe(1);
+      console.info(scopes);
+      let s1 = scopes[0];
+      expect(s1.scope).toBe(SCOPES.VIEW);
+      expect(s1.value).toBe("selectedMovie");
+    });
+  });
+
   describe('parseScope', () => {
     it('should find global', () => {
-      let s = new Scope();
-      let v = s.parseScope("{global:selectedMovie}");
+      let v = Scope.parseScope("{global:selectedMovie}");
       expect(v.scope).toBe(SCOPES.GLOBAL);
       expect(v.value).toBe("selectedMovie");
     });
-    it('should find global', () => {
-      let s = new Scope();
-      let v = s.parseScope("{selectedMovie}");
+    it('should find view', () => {
+      let v = Scope.parseScope("{selectedMovie}");
       expect(v.scope).toBe(SCOPES.VIEW);
       expect(v.value).toBe("selectedMovie");
     });
     it('should find none', () => {
-      let s = new Scope();
-      let v = s.parseScope("selectedMovie");
+      let v = Scope.parseScope("selectedMovie");
       expect(v.scope).toBe(SCOPES.NONE);
       expect(v.value).toBe("selectedMovie");
     });
