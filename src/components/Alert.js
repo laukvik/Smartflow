89ed567cd685fd621de07b7d0bf5bfd6fc4a7980
Morten Laukvik <morten@laukvik.no>
2017-11-11 16:25:01 +0100

    Improved Alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -12,14 +12,20 @@ import {Component} from "../Component";
  */
 
 export const AlertStyle = {
-  INFO: "info",
-  DANGER: "danger",
-  SUCCESS: "success",
-  WARNING: "warning"
+  PRIMARY: "alert-primary",
+  SECONDARY: "alert-secondary",
+  SUCCESS: "alert-success",
+  DANGER: "alert-danger",
+  WARNING: "alert-warning",
+  INFO: "alert-info",
+  LIGHT: "alert-light",
+  DARK: "alert-dark",
 };
 
 /**
  * Alert
+ *
+ * https://getbootstrap.com/docs/4.0/components/alerts/
  *
  * @example
  *
@@ -39,8 +45,10 @@ export class Alert extends Component {
    */
   constructor(properties) {
     super(properties);
+    this._closable = false;
     this.createComponentNode("div");
     this._componentNode.setAttribute("role", "alert");
+
     this._buttonNode = document.createElement("button");
     this._buttonNode.setAttribute("type", "button");
     this._buttonNode.setAttribute("class", "close");
@@ -52,45 +60,51 @@ export class Alert extends Component {
     this._buttonSpanNode = document.createElement("span");
     this._buttonSpanNode.setAttribute("aria-hidden", "true");
     this._buttonSpanNode.innerHTML = "&times;";
-    this._textNode = document.createElement("span");
     this._buttonNode.appendChild(this._buttonSpanNode);
+
+    this._textNode = document.createElement("span");
+    this.getComponentNode().appendChild(this._textNode);
   }
 
   setProperty(name, value) {
-    if (name === "enabled") {
-      this.setEnabled(value);
-    } else if (name === "text") {
+    if (name === "text") {
       this.setText(value);
-    } else if (name === "style") {
-      this.setStyle(value);
+    } else if (name === "alertStyle") {
+      this.setAlertStyle(value);
     } else if (name === "visible") {
       this.setVisible(value);
+    } else if (name === "closable") {
+      this.setClosable(value);
     }
   }
 
-  setStyle(style) {
+  _updateClass(){
+    this._componentNode.setAttribute("class", "alert" + (!this._alertStyle ? "" : " " + this._alertStyle) + (!this._closable ? "" : " alert-dismissible"));
+  }
+
+  setClosable(isClosable){
+    this._closable = isClosable === true;
+    this._updateClass();
+    if (this._closable){
+      this._componentNode.appendChild(this._buttonNode);
+    }
+  }
+
+  setAlertStyle(style) {
     this._alertStyle = style;
-    this._componentNode.setAttribute("class", "alert " + ("alert-" + this._alertStyle));
+    this._updateClass();
   }
 
   buildComponent(builder, properties) {
-    if (properties.closable == true) {
-      this._componentNode.appendChild(this._buttonNode);
-    }
-    this._componentNode.appendChild(this._textNode);
     return this._componentNode;
   }
 
-  setEnabled(isEnabled) {
-    if (isEnabled) {
-      this._componentNode.removeAttribute("disabled");
-    } else {
-      this._componentNode.setAttribute("disabled", "true");
-    }
+  setText(text) {
+    this._textNode.textContent = text;
   }
 
-  setText(text) {
-    this._textNode.innerHTML = text;
+  render(){
+    return this.getComponentNode();
   }
 
 }
